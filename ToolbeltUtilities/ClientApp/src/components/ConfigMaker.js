4ea946fd0a8e6778e87ca0e10c186c81dc9a876a
Ignas Rodžianskis <ignas.rodzianskis@gmail.com>
2020-11-08 14:46:15 +0200

    Added a new tab for specialized configuration maker.

---
+++ ToolbeltUtilities/ClientApp/src/components/ConfigMaker.js
@@ -0,0 +1,31 @@
+﻿import React, { Component } from 'react';
+
+export class ConfigMaker extends Component {
+    static displayName = ConfigMaker.name;
+
+    constructor(props) {
+        super(props);
+        this.state = { currentCount: 0 };
+        this.incrementCounter = this.incrementCounter.bind(this);
+    }
+
+    incrementCounter() {
+        this.setState({
+            currentCount: this.state.currentCount + 1
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>ASF Config Maker</h1>
+
+                <p>This is a simple example of a React component.</p>
+
+                <p aria-live="polite">Current count: <strong>{this.state.currentCount}</strong></p>
+
+                <button className="btn btn-primary" onClick={this.incrementCounter}>Increment</button>
+            </div>
+        );
+    }
+}
